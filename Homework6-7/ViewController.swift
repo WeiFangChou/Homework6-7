f7e8d7e092274ca372d26295e3c172d560344dc5
WeiFang Chou <chou.wei.fang@gmail.com>
2022-07-21 20:47:48 +0800

    Initial Commit

---
+++ Homework6-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework6-7
+//
+//  Created by WeiFangChou on 2022/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
